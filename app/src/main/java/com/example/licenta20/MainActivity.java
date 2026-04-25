f6b04def8e66f3c5ecec570e446623ae839f3c85
gherman <ghermaneddy@gmail.com>
2026-04-25 18:05:26 +0300

    feat: adaugat modelul AppInfo si logica de filtrare a aplicatiilor.

--- app/src/main/java/com/example/licenta20/MainActivity.java
+++ app/src/main/java/com/example/licenta20/MainActivity.java
@@ -1,24 +1,44 @@
 package com.example.licenta20;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Metoda de citire
+        List<AppInfo> installedApps = getInstalledApps();
+    }
+
+    private List<AppInfo> getInstalledApps() {
+        List<AppInfo> appList = new ArrayList<>();
+        PackageManager pm = getPackageManager();
+
+        // Aplicatiile instalate
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo app : packages) {
+            // Verificare de deschidere
+            if (pm.getLaunchIntentForPackage(app.packageName) != null) {
+                String name = app.loadLabel(pm).toString();
+                android.graphics.drawable.Drawable icon = app.loadIcon(pm);
+                appList.add(new AppInfo(name, app.packageName, icon));
+            }
+        }
+
+        // Sortare alfabetica
+        Collections.sort(appList, (a, b) -> a.getName().compareToIgnoreCase(b.getName()));
+
+        return appList;
     }
 }
